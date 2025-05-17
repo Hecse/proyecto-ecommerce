46384fed8914ac421004a57c56985a5c2e56427b
Hector <hecse71@gmail.com>
2025-05-17 14:21:20 -0300

    se hicieron cmbios esteticos

--- scripts/producto.js
+++ scripts/producto.js
@@ -113,7 +113,7 @@ function agregarAlCarrito(unArray) {
 // Pinta la cantidad de productos que hay en el carrito
 function actualizarCantidad() {
   let cantidad = carrito.reduce((acc, producto) => acc + producto.cantidad, 0);
-  console.log(cantidad);
+  //console.log(cantidad);
   carro.innerHTML = ` <h4 class="carro-cantidad">${cantidad}</h4>
 <a href="./carrito.html"><i class="fi fi-rr-shopping-cart"></i></a>`;
 }
